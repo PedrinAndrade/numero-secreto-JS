5be60411c533cbbbf5138301fe30f2e984af8f9e
PedrinAndrade <pedrohandradeg@gmail.com>
2023-11-22 14:14:18 -0300

    Else e Comentários

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 alert('Boas vindas ao jogo do número secreto!');
+
 let numeroSecreto = 5;
+console.log(numeroSecreto);
+
 let chute = prompt('Escolha um número entre 1 e 10');
 
-if(numeroSecreto == chute){
-    console.log('Isso ai! Você descobriu o número secreto (5)')
+//Se o chute for igual ao número secreto
+if (numeroSecreto == chute) {
+    alert('Isso ai! Você descobriu o número secreto (5)');
+
+} else {
+    alert("Você errou!");
 }
