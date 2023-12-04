f4e03f019586e72814213b4de98b8cb26816f1c3
PedrinAndrade <pedrohandradeg@gmail.com>
2023-12-04 10:51:02 -0300

    Loops e tentativas

--- app.js
+++ app.js
@@ -4,19 +4,25 @@ alert(`${nome}, boas vindas ao jogo do número secreto!`);
 let numeroSecreto = 8;
 console.log(numeroSecreto);
 
-let chute = prompt('Escolha um número entre 1 e 10');
-let pontos = 0;
+let chute;
+let tentativas = 1;
 
-//Se o chute for igual ao número secreto
-if (numeroSecreto == chute) {
-    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
-    pontos += 100;
+//Enquanto não for igual ao número secreto
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 10');
 
-    if (pontos >= 100) {
-        alert('Você é o vencedor!');
-    }
+    //Se o chute for igual ao número secreto
+    if (numeroSecreto == chute) {
+        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
     
-} else {
-    alert('Você errou!');
-    alert('Tente ganhar novamente!');
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+        
+        } else {
+            alert(`O número secreto é maior que ${chute}`);
+        }
+        tentativas++;
+    }
 }
+
